51db69b83cfd920ee0386c4b7deefe6c545783de
Karol Nowak <karol.nowak2298@gmail.com>
2024-06-18 22:39:47 +0200

    Remove not used code

--- CannonDefense/Assets/CannonDefense/_Scripts/Game/Battlefield/Logic/IBattlefieldManager.cs
+++ CannonDefense/Assets/CannonDefense/_Scripts/Game/Battlefield/Logic/IBattlefieldManager.cs
@@ -4,8 +4,6 @@ namespace GlassyCode.CannonDefense.Game.Battlefield.Logic
 {
     public interface IBattlefieldManager
     {
-        event Action OnStartBattle;
-        event Action OnEndBattle;
         void StartBattle();
         void EndBattle();
         void RestartBattle();
